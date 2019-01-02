0635b4f7c9a678e3ea59a5871bcf630f096b919d
xltxlm <xltxlm@qq.com>
2019-01-02 11:28:52 +0800

    添加资源类型的定义

---
+++ src/Resources/Define.php
@@ -0,0 +1,9 @@
+<?php
+namespace xltxlm\resources\Resources;
+
+/**
+ * 各个资源的定义;
+*/
+class Define extends Define\Define_implements
+{
+}
---
+++ src/Resources/Define/Define_implements.php
@@ -0,0 +1,22 @@
+<?php
+namespace xltxlm\resources\Resources\Define;
+
+/**
+ * :类;
+ * 外部资源的定义;
+*/
+abstract class Define_implements
+{
+    /*  */
+    public const JIE_KOU="接口";
+    /*  */
+    public const SHU_JU_KU="数据库";
+    /*  */
+    public const ELASTICSEARCH="Elasticsearch";
+    /*  */
+    public const REDIS="Redis";
+    /*  */
+    public const YOU_JIAN="邮件";
+    /*  */
+    public const MQ="MQ";
+}
